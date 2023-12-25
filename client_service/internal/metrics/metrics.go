aa3a374ee96fa27cfae1da6b1483acb9eb13f5e9
IWillBurn <eugene.boyarnikov@gmail.com>
2023-12-25 21:04:02 +0600

    Добавляет новые метрики

--- client_service/internal/metrics/metrics.go
+++ client_service/internal/metrics/metrics.go
@@ -1,1 +1,47 @@
 package metrics
+
+import "github.com/prometheus/client_golang/prometheus"
+
+func init() {
+	prometheus.MustRegister(CreatedOrdersCounter)
+	prometheus.MustRegister(ClientCanceledCounter)
+}
+
+var (
+	CreatedOrdersCounter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "created_orders_counter",
+			Help: "Сounts of created orders",
+		},
+	)
+)
+
+var (
+	ClientCanceledCounter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "client_canceled_counter",
+			Help: "Сounts of orders canceled by client",
+		},
+	)
+)
+
+var (
+	LogoutCounter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "logout_counter",
+			Help: "Сounts of logout operations",
+		},
+	)
+)
+
+func IncreaseSuccessfulLoginsCounter() {
+	SuccessfulLoginsCounter.Inc()
+}
+
+func IncreaseUnsuccessfulLoginsCounter() {
+	UnsuccessfulLoginsCounter.Inc()
+}
+
+func IncreaseLogoutCounter() {
+	LogoutCounter.Inc()
+}
